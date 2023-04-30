bcdfb059eb528855f63c051f8f626c6c8e778c28
EthanReese <ethan.reese01@gmail.com>
2023-04-30 15:52:07 -0400

    Wrote out barebones smart contracts

--- Smart Contracts/contracts/Betting.sol
+++ Smart Contracts/contracts/Betting.sol
@@ -2,34 +2,61 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract Betting {
-        SportsBux private _token;
         address public oracle;
         
-        mapping(string => uint256) public bet_totals;
-        mapping(address => bet) public bets_by_bettor;
+        mapping(string => mapping(bool => uint256)) public bet_totals;
+        // True in the case the home team wins, false in case of away
+        mapping(string => bool) private results; 
+        // True in case that the game has been finished, false otherwise
+        mapping(string => bool) private game_finished;
+        mapping(string => mapping(string => bet)) public bets_by_bettor;
+        
 
         // constructor
         constructor (address tokenAddress, address _oracle) {
-                _token = SportsBux(tokenAddress);
                 oracle = _oracle;
         }
 
-        struct bet {
+        struct Bet {
                 // represents address of person making bet
                 address bettor;
-                // represents the payout if the bet is successful
-                uint256 payout;
+                // represents the amount staked
+                uint256 staked;
                 // represents the game that is being bet on
                 string gameID;
                 // True if the bettor bet on the home team, false otherwise
                 bool home;
         }
 
-        function _placeBet (string gameID, bool home, address bettor, uint256 stake) public view returns (bool){
-                // burns the amount of currency that was bet on the proposition
-                uint256 allowance = allowance(bettor, msg.sender);
-                require(allowance >= stake, "Not enough allowance");
-                _approve(bettor, msg.sender, allowance - stake);
-                _burn(bettor, amount);
+        function placeBet (string gameID, bool home) external payable{
+                // confirm that the game hasn't happened already
+                require(game_finished[gameID] == false, "The game is over");
+                // burns the amount of currency that was bet on the proposition                
+                Bet bet = new Bet(msg.sender, msg.value, gameID, home);
+                // flaw: this only allows the user to bet one side of the game
+                bets_by_bettor[msg.sender][gameID] = bet;
+                bets[gameID][home] += msg.value;
+        }
+
+        function withdrawGain (string gameID) external {
+                // confirm that the game has already happened
+                require(game_finished[gameID] == true, "The game hasn't happened yet");
+                
+                bool winner = results[gameID];
+                Bet bet = bets_by_bettor[msg.sender][gameID];
+                // amount that the bettor wins
+                if (bet.home == winner){
+                        uint256 win_amount = bet.staked + bet_totals[gameID][!winner] * bet.staked / bet_totals[gameID][winner];
+                }
+                else {
+                        uint256 win_amount = 0;
+                }
+                msg.sender.transfer(win_amount);
+        }
+
+        function reportResult(string gameID, bool home) external {
+                require(oracle == msg.sender, "Only the Oracle can call this function");
+                game_finished[gameID] = True;
+                results[gameID] = home;
         }
 }
