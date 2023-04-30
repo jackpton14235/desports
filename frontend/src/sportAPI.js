538654a59ca0b31e83be518aad5fde5269a44626
jacks14235 <jack@stanley5.com>
2023-04-30 19:42:52 -0400

    add requests for 7 days

--- frontend/src/sportAPI.js
+++ frontend/src/sportAPI.js
@@ -1,38 +1,54 @@
-// Constants
-var myHeaders = new Headers();
-var games = [];
-var date = new Date();
-let year;
-let month;
-let day;
-myHeaders.append("x-rapidapi-key", "eefc3ede4e8d6b86b69a21dc267eb6cc");
-myHeaders.append("x-rapidapi-host", "v1.baseball.api-sports.io");
+export function getThisWeek() {
+  // Constants
+  const ms_per_day = 86_400_000;
+  var myHeaders = new Headers();
+  myHeaders.append("x-rapidapi-key", "eefc3ede4e8d6b86b69a21dc267eb6cc");
+  myHeaders.append("x-rapidapi-host", "v1.baseball.api-sports.io");
 
-for(let i = 0; i < 1; i++){
-    year = date.getFullYear(); 
-    month = String(date.getMonth()+1).padStart(2,"0");
-    day = date.getDate() - i;
-    day = String(day).padStart(2, '0');
+  // for(let i = 0; i < 1; i++){
+  //     year = date.getFullYear();
+  //     month = String(date.getMonth()+1).padStart(2,"0");
+  //     day = date.getDate() - i;
+  //     day = String(day).padStart(2, '0');
+  //     let currentDate = `${year}-${month}-${day}`;
+
+  //     const myParameters = new URLSearchParams({
+  //         date: currentDate,
+  //         league: "1",
+  //         season: "2023"
+  //     });
+
+  const requestOptions = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  // get datetimes for last 7 days
+  const now = Date.now();
+  const dates = new Array(7).fill(0).map((_, i) => new Date(now - i * ms_per_day));
+
+  // get list of requests for each day
+  const requests = dates.map((date) => {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    let day = String(date.getDate()).padStart(2, "0");
     let currentDate = `${year}-${month}-${day}`;
-
-    var myParameters = new URLSearchParams({
-        date: currentDate,
-        league: "1",
-        season: "2023"
+    const params = new URLSearchParams({
+      date: currentDate,
+      league: "1",
+      season: "2023",
     });
+    return fetch(
+      "https://v1.baseball.api-sports.io/games?" + params,
+      requestOptions
+    );
+  });
 
-    var requestOptions = {
-        method: 'GET',
-        headers: myHeaders,
-        redirect: 'follow'
-      };
-      
-      fetch("https://v1.baseball.api-sports.io/games?" + myParameters, requestOptions)
-        .then(response => response.text())
-        .then(result => {
-          var game = JSON.parse(result);
-          games.push(game);
-          console.log(game);
-        })
-        .catch(error => console.log('error', error));
+  // get all requests concurrently
+  Promise.all(requests)
+    .then((results) => {
+      console.log(results);
+    })
+    .catch((err) => console.error(err));
 }
