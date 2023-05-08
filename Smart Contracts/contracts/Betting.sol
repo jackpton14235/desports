d625f25ad07962019b2e66119b7bcb825bd9d052
EthanReese <ethan.reese01@gmail.com>
2023-05-08 12:35:30 -0400

    Made changes to smart contract

--- Smart Contracts/contracts/Betting.sol
+++ Smart Contracts/contracts/Betting.sol
@@ -4,12 +4,12 @@ pragma solidity >=0.4.22 <0.9.0;
 contract Betting {
         address public oracle;
         
-        mapping(string => mapping(bool => uint256)) public bet_totals;
+        mapping(uint256 => mapping(bool => uint256)) public bet_totals;
         // True in the case the home team wins, false in case of away
-        mapping(string => bool) private results; 
+        mapping(uint256 => bool) private results; 
         // True in case that the game has been finished, false otherwise
-        mapping(string => bool) private game_finished;
-        mapping(address => mapping(string => Bet)) public bets_by_bettor;
+        mapping(uint256 => bool) private game_finished;
+        mapping(address => Bet[]) public bets_by_bettor;
         
 
         // constructor
@@ -23,31 +23,39 @@ contract Betting {
                 // represents the amount staked
                 uint256 staked;
                 // represents the game that is being bet on
-                string gameID;
+                uint256 gameID;
                 // True if the bettor bet on the home team, false otherwise
                 bool home;
+                // initialization check
+                bool initialized;
         }
 
-        function getBets (string memory gameID) public view returns (uint256, uint256) {
+        function getBets (uint256 gameID) public view returns (uint256, uint256) {
                 require(game_finished[gameID] == false, "The game is over or doesn't exist");
                 return (bet_totals[gameID][true], bet_totals[gameID][false]);
         }
 
-        function placeBet (string memory gameID, bool home) external payable{
+        function placeBet (uint256 gameID, bool home) external payable{
                 // confirm that the game hasn't happened already
                 require(game_finished[gameID] == false, "The game is over");
                 // flaw: this only allows the user to bet one side of the game
-                bets_by_bettor[msg.sender][gameID] = 
-                        Bet(msg.sender, msg.value, gameID, home);
+                bets_by_bettor[msg.sender].push(Bet(msg.sender, msg.value, gameID, home));
                 bet_totals[gameID][home] += msg.value;
         }
 
-        function withdrawGain (string memory gameID) external {
+        function withdrawGain (uint256 gameID) external {
                 // confirm that the game has already happened
                 require(game_finished[gameID] == true, "The game hasn't happened yet");
                 
                 bool winner = results[gameID];
-                Bet memory bet = bets_by_bettor[msg.sender][gameID];
+                Bet memory bet;
+                for (uint i = 0; i < bets_by_bettor[msg.sender].length; i++){
+                        if (bets_by_bettor[msg.sender][i].gameID == gameID)
+                        {
+                                bet = bets_by_bettor[msg.sender][i];
+                        }
+                }
+                require(!bet.initialized, "There is no bet on the game");
                 // amount that the bettor wins
                 uint256 win_amount = 0;
                 if (bet.home == winner){
@@ -59,7 +67,7 @@ contract Betting {
                 payable(msg.sender).transfer(win_amount);
         }
 
-        function reportResult(string memory gameID, bool home) external {
+        function reportResult(uint256 gameID, bool home) external {
                 require(oracle == msg.sender, "Only the Oracle can call this function");
                 game_finished[gameID] = true;
                 results[gameID] = home;
