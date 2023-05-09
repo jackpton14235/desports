a595c89df47136b88b62a05c3cb50a5e5bcd15b7
EthanReese <ethan.reese01@gmail.com>
2023-05-09 11:24:27 -0400

    Added loop over for payouts

--- Smart Contracts/contracts/Betting.sol
+++ Smart Contracts/contracts/Betting.sol
@@ -16,6 +16,9 @@ contract Betting is ChainlinkClient, ConfirmedOwner {
     // True in case that the game has been finished, false otherwise
     mapping(uint256 => bool) private game_finished;
     mapping(address => Bet[]) public bets_by_bettor;
+    
+    // maps a game id to an array of bettors that bet on the game
+    mapping(uint256 => address[]) public bettors_by_game;
 
     uint256 public volume;
     bytes32 private jobId;
@@ -54,6 +57,7 @@ contract Betting is ChainlinkClient, ConfirmedOwner {
         // flaw: this only allows the user to bet one side of the game
         bets_by_bettor[msg.sender].push(Bet(msg.sender, msg.value, gameID, home, true));
         bet_totals[gameID][home] += msg.value;
+        bettors_by_game[gameID].push(msg.sender);
     }
 
     // calls chailink oracle to get game status from our external adapter
@@ -85,35 +89,44 @@ contract Betting is ChainlinkClient, ConfirmedOwner {
             return;
         } else if (info < 0) {
             // away won
-            game_finished[uint256(info * -1)] = false;
+            uint256 gameID = uint256(info * -1);
+            game_finished[gameID] = true;
+            results[gameID] = false;
+            withdrawGain(gameID, false);
+
         } else {
             // home won
-            game_finished[uint256(info)] = false;
+            uint256 gameID = uint256(info);
+            game_finished[gameID] = true;
+            results[gameID] = true;
+                withdrawGain(gameID, true);
         }
+
     }
 
-    function withdrawGain (uint256 gameID) external {
-        // confirm that the game has already happened
-        require(game_finished[gameID] == true, "The game hasn't happened yet");
-        
-        bool winner = results[gameID];
+    function withdrawGain (uint256 gameID, bool winner) internal {
         Bet memory bet;
-        for (uint i = 0; i < bets_by_bettor[msg.sender].length; i++){
-                if (bets_by_bettor[msg.sender][i].gameID == gameID)
-                {
-                        bet = bets_by_bettor[msg.sender][i];
+        for (uint j = 0; j < bettors_by_game[gameID].length; j++){
+                address bettor = bettors_by_game[gameID][j];
+                for (uint i = 0; i < bets_by_bettor[bettor].length; i++){
+                        if (bets_by_bettor[bettor][i].gameID == gameID)
+                        {
+                                bet = bets_by_bettor[bettor][i];
+                        }
                 }
+                // require(!bet.initialized, "There is no bet on the game");
+                // amount that the bettor wins
+                uint256 win_amount = 0;
+                if (bet.home == winner){
+                        win_amount = bet.staked + bet_totals[gameID][!winner] 
+                                * bet.staked / bet_totals[gameID][winner];
+                }
+                else {
+                        win_amount = 0;
+                }
+                payable(bettor).transfer(win_amount);
         }
-        require(!bet.initialized, "There is no bet on the game");
-        // amount that the bettor wins
-        uint256 win_amount = 0;
-        if (bet.home == winner){
-                win_amount = bet.staked + bet_totals[gameID][!winner] * bet.staked / bet_totals[gameID][winner];
-        }
-        else {
-                win_amount = 0;
-        }
-        payable(msg.sender).transfer(win_amount);
+        
     }
 
     function reportResult(uint256 gameID, bool home) external {
